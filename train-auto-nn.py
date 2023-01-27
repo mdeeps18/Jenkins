fa343cef10908f8a84c40e5db7fa00bb709dfc7e
Devendra61 <52621589+Devendra61@users.noreply.github.com>
2023-01-28 01:22:12 +0530

    Update train-auto-nn.py

--- train-auto-nn.py
+++ train-auto-nn.py
@@ -53,7 +53,7 @@ def train():
          'hidden_layer_sizes': [(300,),(500,)],
          'max_iter': [1000],
          'alpha': [1e-5, 0.001, 0.01, 0.1, 1, 10],
-         'random_state':[0]
+         'random_state':[1]
      }
     ]
 
